8c15525d2241227363de6f102ce98dbb6fa0d9d3
Chiemezie <nwachieme2@gmail.com>
2025-09-15 06:45:52 +0100

    File Upload
    uploading of website file to repository

---
+++ scripts/blogScript.js
@@ -0,0 +1,56 @@
+const overView = {
+    registration: "Candidates register via accredited CBT centers with biometric data and personal information",
+    selectInstitute: "You can choose up to four institutions — a mix of universities, polytechnics, and colleges.",
+    Examination:"Use of English (compulsory)<br/>Three other subjects based on your intended course of study.",
+    releaseResult: "UTME results are typically released within 48–72 hours of the exam.",
+    postUTME: "After UTME, many universities conduct their own post-UTME screening or exams.",
+    CAPS_Admission: "The Central Admissions Processing System (CAPS) is the official portal where students can accept or reject admission offers"
+}
+
+console.log(typeof (document.querySelector('.link')));
+
+const navlink = document.querySelector('.link');
+
+
+
+function sayHello (){
+    console.log('hello');
+}
+
+function whatsAppy(){
+    let phoneNumber = "2347061473780";
+    let url = "https://wa.me/" + phoneNumber;
+    window.open(url, "_blank");
+}
+
+const navButton = document.querySelector('.hamburger');
+
+console.log(navButton);
+
+let droppedDown = false;
+
+
+const dropMenu = function(){
+    if (!droppedDown) {
+        document.querySelector('.center').classList.add('disappear');
+        console.log(document.querySelector('.center').classList);
+        document.querySelector('.top').classList.add("topper");
+        document.querySelector('.bottom').classList.add('bottomer');
+
+        //drop down
+        document.querySelector('.nav-list').classList.add('dropDown')
+        
+        droppedDown = true;
+    }
+        else if(droppedDown) {
+        document.querySelector('.center').classList.remove('disappear');
+        document.querySelector('.top').classList.remove("topper");
+        document.querySelector('.bottom').classList.remove('bottomer');
+
+        //drop it down
+        document.querySelector('.nav-list').classList.remove('dropDown'); 
+        droppedDown = false;
+    }
+}
+
+navButton.addEventListener('click', dropMenu);
